33a452012f27289315cf380d8abeb496edd6e48f
Joshua Blair <joshua.blair@csi.corp>
2017-06-22 11:20:35 -0400

    module 9 work in progress travis ci test

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './index.css';
+
+import numeral from 'numeral';
+
+const courseValue = numeral(1000).format('$0,0.00');// eslint-disable-line no-unused-vars
+console.log('I would pay ${courseValue} for this awesom course!');// eslint-disable-line no-console
